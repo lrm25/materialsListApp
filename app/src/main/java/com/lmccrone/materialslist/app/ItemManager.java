c968c95b557b41761599753c9ae2b548e0474d2a
lrm25 <luke.r.mccrone@gmail.com>
2019-01-24 20:52:27 -0700

    Added comments to ItemManager

--- app/src/main/java/com/lmccrone/materialslist/app/ItemManager.java
+++ app/src/main/java/com/lmccrone/materialslist/app/ItemManager.java
@@ -11,6 +11,9 @@ public class ItemManager {
     private AllItemList itemList;
     private static ItemManager itemManager;
 
+    /**
+     * Return singleton 
+     */
     public static ItemManager instance() {
         if (itemManager == null) {
             itemManager = new ItemManager();
@@ -18,20 +21,34 @@ public class ItemManager {
         return itemManager;
     }
 
+    /**
+     * Constructor
+     */
     private ItemManager() {
         itemList = new AllItemList();
     }
 
+    /**
+     * Add an item to the app's stored item
+     * throws ItemException:  item already exists, has empty name, etc.
+     */
     public void addItem(String item) throws ItemException {
 
         itemList.add(new Item(item));
     }
 
+    /**
+     * Remove item from list
+     * TODO:  add exception
+     */
     public void deleteItem(Item item) {
 
         itemList.delete(item);
     }
 
+    /**
+     * Get the full list of itmes stored on the app
+     */
     public ArrayList<Item> getItemList() {
         return itemList.getList();
     }
